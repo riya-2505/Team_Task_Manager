1906c197b1abc3edfa5bb15a06f3458aa8bb9550
Your Name <you@example.com>
2026-05-03 21:35:55 +0530

    connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL:"https://teamtaskmanager-production-b83b.up.railway.app/api"
 });
 
 // Add token automatically
